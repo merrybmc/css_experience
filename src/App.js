227f879df192385a439bafa2a2f23c34edf76568
Byeong Min Cho <merrybmc@gmail.com>
2023-05-25 23:54:12 +0900

    css 체험

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import TailwindComponent from './TailwindComponent';
+import Button1 from './components/Button1';
+import Button2 from './components/Button2';
+import styled from 'styled-components';
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+      <Container />
+      <TailwindComponent />
+    </>
+  );
 }
 
 export default App;
+
+// styled components
+
+export const Container = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 5px;
+  border: 1px solid;
+`;
---
+++ src/components/Button2.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Button2.module.css';
+
+// PostCSS
+
+export default function Button2() {
+  return <button className={styles.button}>Button2</button>;
+}
